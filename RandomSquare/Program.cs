3532bdfdbacc1092928ee7af877c6002d1749492
RichFisher <rich.fishernc@gmail.com>
2019-03-11 19:50:45 -0500

    its rando

--- RandomSquare/Program.cs
+++ RandomSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomSquare
 {
@@ -6,7 +7,31 @@ namespace RandomSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var randy = new Random();
+            var randomList = new List<int>();
+            for (var i = 0; i < 20; i++)
+            {
+                randomList.Add(randy.Next(1, 50));
+            }
+
+            var squared = new List<int>();
+            foreach (int num in randomList)
+            {
+                var squaredNum = num * num;
+                var squaredDec = Convert.ToDecimal(squaredNum);
+                
+                if ((squaredDec/ 2) == Math.Floor(squaredDec / 2 ))
+                {
+                    squared.Add(squaredNum);
+                }          
+            }
+
+            foreach(int num in squared)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
         }
     }
 }
